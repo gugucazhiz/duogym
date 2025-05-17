9507e725a542ce935aba3050f085617230eb40b6
gugucazhiz <domateklh@gmail.com>
2025-05-17 18:42:52 -0300

    update 2

--- backend/server.js
+++ backend/server.js
@@ -10,36 +10,66 @@ app.use(cors());
 app.use(express.json());
 const users = [];
 
+
+////////////     REGISTRO
+
 app.post("/api/register", async(req,res) =>{
-
-    const{ email, password } = req.body;
-
-    const userExists = users.find(u => u.email === email);
-    if(userExists){
-        return res.status(400).json({ message: "Usuario Ja Existe"});
+    try{
+        const{ email, password } = req.body;
+        
+        if(!email || !password){
+                return res.status(400).json({ message: "Email e Senha Obrigatorios"});
+            }
+        
+        const userExists = users.find(u => u.email === email);
+        if(userExists){
+            return res.status(400).json({ message: "Usuario Ja Existe"});
+        }
+    
+        //criptografia
+    
+        const hashedPassword = await bcrypt.hash(password, 8);
+    
+        users.push({email, password: hashedPassword});
+    
+        const token = jwt.sign({email}, "segredo", { expiresIn: "1h"});
+    
+        res.status(201).json({token});
     }
-
-    //criptografia
-
-    const hashedPassword = await bcrypt.hash(password, 8);
-
-    users.push({email, password: hashedPassword});
-
-    const token = jwt.sign({email}, "segredo", { expiresIn: "1h"});
-
-    res.status(201).json({token});
+    catch(error){
+        console.error("Erro ao registrar: ",error);
+        res.status(500).json({ message: "Erro interno no servidor "});
+    }
 });
 
-app.post("api/login",async(req,res) =>{
+/////////////////// LOGIN
+
+app.post("/api/login",async(req,res) =>{
     try{
         const{email, password}= req.body;
 
-        if(!email || !password){
-            return res.status(400).json({ message: "Email e Senha Obrigatorios"});
+        //verifica se existe
+
+        const user = users.find(u => u.email === email);
+        if(!user){
+            return res.status(400).json({ message: "Usuario não encontrado: "});
         }
 
-        const userExists
-    }catch(error){}
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if(!passwordMatch){
+            return res.status(401).json({ message: "Senha Incorreta. "});
+        }
+
+        //gera token
+        const token = jwt.sign({ email}, "Segredo", { expiresIn: "1h"});
+
+        //retorna
+        res.json({token});
+
+    }catch(error){
+        console.error("Erro ao fazer login: ",error);
+        res.status(500).json({ message: "Error interno no servidor: "});
+    }
 });
 
 const PORT = 3000;
